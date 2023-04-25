fc90634410a0500fa47c7f49f7be8f002943eb4b
nilsdev <nilsdev+1@protonmail.com>
2023-04-25 14:43:57 +0200

    removed comments from my copy pasting spree

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,7 @@ use gtk::{
     gdk::Display, prelude::*, ApplicationWindow, Button, CssProvider, Orientation, StyleContext,
 };
 
-// https://github.com/wmww/gtk-layer-shell/blob/master/examples/simple-example.c
 fn activate(application: &gtk::Application) {
-    // Create a normal GTK window however you like
-    // let window = gtk::ApplicationWindow::new(application);
-
     let gtk_box = gtk::Box::builder()
         .orientation(Orientation::Horizontal)
         .halign(gtk::Align::End)
@@ -22,20 +18,12 @@ fn activate(application: &gtk::Application) {
         .label("Press me!")
         .margin_top(12)
         .margin_bottom(12)
-        // .margin_start(12)
-        // .margin_end(12)
         .build();
 
     let (sender, receiver) = MainContext::channel(PRIORITY_DEFAULT);
-    // Connect to "clicked" signal of `button`
     button.connect_clicked(move |_| {
         let sender = sender.clone();
-        // The long running operation runs now in a separate thread
         thread::spawn(move || {
-            // Deactivate the button until the operation is done
-            // sender.send(10).expect("Could not send through channel");
-            // let ten_seconds = Duration::from_secs(10);
-
             let mut counter = 0;
             while counter < 1000 {
                 thread::sleep(Duration::from_millis(100));
@@ -44,16 +32,13 @@ fn activate(application: &gtk::Application) {
                     .send(1000 - counter)
                     .expect("Could not send through channel");
             }
-            // Activate the button again
         });
     });
 
-    // The main loop executes the closure as soon as it receives the message
     receiver.attach(
         None,
         clone!(@weak button => @default-return Continue(false),
                     move |sender_message| {
-                        // button.set_sensitive(enable_button);
                         button.set_label(&sender_message.to_string());
                         Continue(true)
                     }
@@ -75,7 +60,6 @@ fn activate(application: &gtk::Application) {
         None,
         clone!(@weak date_label => @default-return Continue(false),
                     move |sender_message| {
-                        // button.set_sensitive(enable_button);
                         date_label.set_label(&sender_message.to_string());
                         Continue(true)
                     }
@@ -114,14 +98,12 @@ fn activate(application: &gtk::Application) {
         None,
         clone!(@weak battery_label => @default-return Continue(false),
                     move |sender_message| {
-                        // button.set_sensitive(enable_button);
                         battery_label.set_label(&sender_message.to_string());
                         Continue(true)
                     }
         ),
     );
 
-    // gtk_box.append(&button);
     gtk_box.append(&battery_label);
     gtk_box.append(&date_label);
 
@@ -132,26 +114,19 @@ fn activate(application: &gtk::Application) {
         .child(&gtk_box)
         .build();
 
-    // Before the window is first realized, set it up to be a layer surface
     gtk4_layer_shell::init_for_window(&window);
 
-    // Display above normal windows
     gtk4_layer_shell::set_layer(&window, gtk4_layer_shell::Layer::Top);
 
-    // Push other windows out of the way
     gtk4_layer_shell::auto_exclusive_zone_enable(&window);
 
     gtk4_layer_shell::set_namespace(&window, "wayland-shell");
 
-    // The margins are the gaps around the window's edges
-    // Margins and anchors can be set like this...
     gtk4_layer_shell::set_margin(&window, gtk4_layer_shell::Edge::Left, 0);
     gtk4_layer_shell::set_margin(&window, gtk4_layer_shell::Edge::Right, 0);
     gtk4_layer_shell::set_margin(&window, gtk4_layer_shell::Edge::Top, 0);
     gtk4_layer_shell::set_margin(&window, gtk4_layer_shell::Edge::Bottom, 0);
 
-    // ... or like this
-    // Anchors are if the window is pinned to each edge of the output
     let anchors = [
         (gtk4_layer_shell::Edge::Left, true),
         (gtk4_layer_shell::Edge::Right, true),
@@ -163,23 +138,13 @@ fn activate(application: &gtk::Application) {
         gtk4_layer_shell::set_anchor(&window, anchor, state);
     }
 
-    // Set up a widget
-    // let label = gtk::Label::new(Some(""));
-    // label.set_markup("<span font_desc=\"20.0\">GTK Layer Shell example!</span>");
-    // window.set_child(Some(&label));
-
-    // let test = gtk::Label::new(Some("asdasd"));
-    // window.set_child(Some(&test));
-
     window.show()
 }
 
 fn load_css() {
-    // Load the CSS file and add it to the provider
     let provider = CssProvider::new();
     provider.load_from_data(include_str!("style.css"));
 
-    // Add the provider to the default screen
     StyleContext::add_provider_for_display(
         &Display::default().expect("Could not connect to a display."),
         &provider,
